d200038b051ffb71843584b1ded7196f1425090f
Dicky Setiawan <111695964+KarpetBasah@users.noreply.github.com>
2024-09-20 22:56:41 +0800

    Added hyperlink function

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 document.addEventListener('scroll', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('.nav-link');
-  
-    let currentSection = '';
-  
-    sections.forEach(section => {
-      const sectionTop = section.offsetTop;
-      if (pageYOffset >= sectionTop - 150) {  // Adjust the offset as needed
-        currentSection = section.getAttribute('id');
-      }
-    });
-  
-    navLinks.forEach(link => {
-      link.classList.remove('active');
-      if (link.getAttribute('href').substring(1) === currentSection) {
-        link.classList.add('active');
-      }
-    });
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  let currentSection = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 150) {  // Adjust the offset as needed
+      currentSection = section.getAttribute('id');
+    }
   });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href').substring(1) === currentSection) {
+      link.classList.add('active');
+    }
+  });
+});
+
+function scrollToSection(event, target) {
+  event.preventDefault();
+  const navbar = document.querySelector('.navbar');
+  const navbarHeight = navbar.offsetHeight;
+  const element = document.querySelector(target);
+  const y = element.getBoundingClientRect().top + window.pageYOffset - navbarHeight;
+  window.scrollTo({ top: y, behavior: 'smooth' });
+}
